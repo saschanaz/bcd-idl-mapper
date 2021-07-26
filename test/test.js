604f833b8ac1b5fd66db6764074c29db92f84363
Kagami Sascha Rosylight <saschanaz@outlook.com>
2021-07-26 04:04:36 +0200

    fix: mixin should be object to keep bcd compatible structure

--- test/test.js
+++ test/test.js
@@ -3,36 +3,37 @@ import expect from "expect";
 
 describe("existence", () => {
   it("api.Element.webkitMatchesSelector", () => {
-    expect(api.Element.webkitMatchesSelector).toBeDefined();
     expect(api.Element.webkitMatchesSelector.__compat.support).toBeDefined();
   });
   it("api.__mixins.ParentNode", () => {
     expect(api.__mixins.ParentNode).toBeDefined();
-    expect(api.__mixins.ParentNode.__compat.support).toBeDefined();
   });
   it("api.CSSStyleDeclaration.gridTemplate", () => {
-    expect(api.CSSStyleDeclaration.gridTemplate).toBeDefined();
     expect(api.CSSStyleDeclaration.gridTemplate.__compat.support).toBeDefined();
   });
   it("api.CSSStyleDeclaration.wordWrap", () => {
-    expect(api.CSSStyleDeclaration.wordWrap).toBeDefined();
     expect(api.CSSStyleDeclaration.wordWrap.__compat.support).toBeDefined();
   });
 
   // TODO: api.CSSStyleDeclaration.gridGap
 });
 
+describe("structure", () => {
+  it("mixin support is a record", () => {
+    expect(
+      api.__mixins.ParentNode.append.Document.__compat.support
+    ).toBeDefined();
+  });
+});
+
 describe("original data", () => {
   it("api.DOMException", () => {
-    expect(api.DOMException).toBeDefined();
     expect(api.DOMException.__compat.support).toBeDefined();
   });
   it("api.CSSStyleDeclaration.overflowWrap", () => {
-    expect(api.CSSStyleDeclaration.overflowWrap).toBeDefined();
     expect(api.CSSStyleDeclaration.overflowWrap.__compat.support).toBeDefined();
   });
   it("api.Event", () => {
-    expect(api.Event).toBeDefined();
     expect(api.Event.__compat.support).toBeDefined();
   });
 });
